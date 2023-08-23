cff189a1ac153271b3f2a211a5747ce68a3d0780
atanas-hristozov <64282150+atanas-hristozov@users.noreply.github.com>
2023-08-23 16:32:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,21 +16,4 @@ public class Main {
         stack.pop();
         System.out.println(stack);
     }
-        /*
-        List<Integer> list = new ArrayList<>(4);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(4,0);
-        System.out.println(list.get(4));
-        list.remove(0);
-        list.remove(list.size()-1);
-        list.contains(3);
-        for (int i = 0; i< list.size()-1; i++){
-            System.out.println(list.get(i));
-        }
-    }*/
 }
